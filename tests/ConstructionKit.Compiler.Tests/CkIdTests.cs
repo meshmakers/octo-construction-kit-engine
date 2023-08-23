0996344bca66b8e012a59c311f6e42101d85bded
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-08-23 16:35:15 +0200

    Cleanup

---
+++ tests/ConstructionKit.Compiler.Tests/CkIdTests.cs
@@ -0,0 +1,22 @@
+using Meshmakers.Octo.ConstructionKit.Contracts;
+
+namespace Meshmakers.Octo.ConstructionKit.Compiler.Tests;
+
+public class CkIdTests
+{
+    [Fact]
+    public void Copy_CkId()
+    {
+        var ckTypeId = new CkId<CkTypeId>("System/Designation-1.0.0");
+        var ckTypeId2 = ckTypeId;
+        test(ckTypeId2);
+    }
+
+    private void test(CkId<CkTypeId> ckTypeId)
+    {
+        Assert.Equal("System", ckTypeId.ModelId.ModelId);
+        Assert.Equal("Designation", ckTypeId.Key.TypeId);
+        Assert.Equal("1.0.0", ckTypeId.ModelId.ModelVersion);
+        Assert.Equal("1.0.0", ckTypeId.Key.Version);
+    }
+}

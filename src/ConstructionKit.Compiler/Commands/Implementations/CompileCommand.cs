d1cd1b5c1aca8dcbda17e732d02d9c806a903f9f
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-09-26 04:49:55 +0200

    Added files of runtime entity validation Added compiler features to publish to local repository.

--- src/ConstructionKit.Compiler/Commands/Implementations/CompileCommand.cs
+++ src/ConstructionKit.Compiler/Commands/Implementations/CompileCommand.cs
@@ -1,5 +1,7 @@
 ﻿using Meshmakers.Common.CommandLineParser;
 using Meshmakers.Common.CommandLineParser.Commands;
+using Meshmakers.Octo.ConstructionKit.Contracts;
+using Meshmakers.Octo.ConstructionKit.Contracts.Serialization;
 using Meshmakers.Octo.ConstructionKit.Contracts.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -9,14 +11,19 @@ namespace Meshmakers.Octo.ConstructionKit.Compiler.Commands.Implementations;
 internal class CompileCommand : Command<OctoToolOptions>
 {
     private readonly ICompilerService _compilerService;
+    private readonly ICkSerializer _ckSerializer;
+    private readonly ICkModelRepositoryService _ckModelRepositoryService;
     private readonly IArgument _pathArg;
     private readonly IArgument _cacheArg;
+    private readonly IArgument _publishArg;
 
     public CompileCommand(ILogger<CompileCommand> logger, IOptions<OctoToolOptions> options,
-        ICompilerService compilerService)
+        ICompilerService compilerService, ICkSerializer ckSerializer, ICkModelRepositoryService ckModelRepositoryService)
         : base(logger, "Compile", "Validates and creates output files for a construction kit model directory", options)
     {
         _compilerService = compilerService;
+        _ckSerializer = ckSerializer;
+        _ckModelRepositoryService = ckModelRepositoryService;
 
         _pathArg = CommandArgumentValue.AddArgument("p", "path",
             new[] { "Root path of construction kit model directory" }, true, 1);
@@ -24,6 +31,9 @@ internal class CompileCommand : Command<OctoToolOptions>
         _cacheArg = CommandArgumentValue.AddArgument("c", "cache",
             new[] { "If used, parallel to the compiled construction kit model a cache file is created containing " +
                     "all dependent construction kit models." }, false);
+        
+        _publishArg = CommandArgumentValue.AddArgument("i", "import",
+            new[] { "When set, the compiled file gets imported to the local repository." }, false);
     }
 
     public override async Task Execute()
@@ -32,9 +42,27 @@ internal class CompileCommand : Command<OctoToolOptions>
 
         var rootPath = CommandArgumentValue.GetArgumentScalarValue<string>(_pathArg);
         var createCacheFile = CommandArgumentValue.IsArgumentUsed(_cacheArg);
+        var doPublish = CommandArgumentValue.IsArgumentUsed(_publishArg);
         Logger.LogInformation("Path of root directory: {Path}", Path.GetFullPath(rootPath));
 
-        await _compilerService.CompileAsync(rootPath, createCacheFile);
+        var compiledModelFilePath = await _compilerService.CompileAsync(rootPath, createCacheFile);
+
+        if (doPublish)
+        {
+            var operationResult = new OperationResult();
+            await using var streamReader = File.OpenRead(compiledModelFilePath);
+            
+            var ckCompiledModelRoot = await _ckSerializer.DeserializeCompiledModelRootAsync(streamReader, compiledModelFilePath, operationResult);
+            if (operationResult.HasErrors)
+            {
+                Logger.LogError("Error loading model \'{FilePath}\'", compiledModelFilePath);
+                operationResult.WriteMessagesToLogger(Logger);
+                return;
+            }
+
+            Logger.LogInformation("Publishing construction kit model to 'LocalRepository'");
+            await _ckModelRepositoryService.PublishModelAsync("LocalRepository", ckCompiledModelRoot, true);
+        }
         
         Logger.LogInformation("Construction kit model directory compiled");
     }
---
+++ src/Runtime.Contracts/CurrentMultiplicity.cs
@@ -0,0 +1,8 @@
+namespace Meshmakers.Octo.SystematizedData.Persistence.DataAccess;
+
+public enum CurrentMultiplicity
+{
+    Zero = 0,
+    One = 1,
+    Many = 2
+}

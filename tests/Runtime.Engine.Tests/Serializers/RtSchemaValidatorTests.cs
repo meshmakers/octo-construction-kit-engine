217cfbadd2b1fef7cac1d8a042faa02dcb2a36ac
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-09-27 07:56:55 +0200

    Added further tests for runtime validator, yaml serializer

---
+++ tests/Runtime.Engine.Tests/Serializers/RtSchemaValidatorTests.cs
@@ -0,0 +1,105 @@
+using Meshmakers.Octo.ConstructionKit.Contracts;
+using Meshmakers.Octo.Runtime.Engine.Serialization;
+
+namespace Meshmakers.Octo.Runtime.Engine.Tests.Serializers;
+
+public class RtSchemaValidatorTests
+{
+    
+    [Fact]
+    public void ValidateModelInJson_ok()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/entity-ok.json";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        bool isValid = schemaValidator.ValidateModelInJson(stream, filePath, operationResult);
+        Assert.True(isValid);
+        Assert.False(operationResult.Messages.Any());
+        Assert.False(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+    }
+    
+    [Fact]
+    public void ValidateModelInJson_MalformedAttribute_Fail()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/malformedAttribute.json";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        var isValid = schemaValidator.ValidateModelInJson(stream, filePath, operationResult);
+        Assert.False(isValid);
+        Assert.Equal(2, operationResult.Messages.Count);
+        Assert.True(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+        Assert.Equal(1, operationResult.Messages[0].MessageNumber);
+        Assert.Equal(1, operationResult.Messages[1].MessageNumber);
+    }
+    
+    [Fact]
+    public void ValidateModelInJson_MalformedAttributeValue_Fail()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/malformedAttributeValue.json";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        var isValid = schemaValidator.ValidateModelInJson(stream, filePath, operationResult);
+        Assert.False(isValid);
+        Assert.Single(operationResult.Messages);
+        Assert.True(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+        Assert.Equal(1, operationResult.Messages[0].MessageNumber);
+    }
+    
+    
+    [Fact]
+    public void ValidateModelInYaml_ok()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/entity-ok.yaml";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        var isValid = schemaValidator.ValidateModelInYaml(stream, filePath, operationResult);
+        Assert.True(isValid);
+        Assert.False(operationResult.Messages.Any());
+        Assert.False(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+    }
+    
+    [Fact]
+    public void ValidateModelInYaml_MalformedAttribute_Fail()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/malformedAttribute.yaml";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        var isValid = schemaValidator.ValidateModelInYaml(stream, filePath, operationResult);
+        Assert.False(isValid);
+        Assert.Equal(2, operationResult.Messages.Count);
+        Assert.True(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+        Assert.Equal(1, operationResult.Messages[0].MessageNumber);
+        Assert.Equal(1, operationResult.Messages[1].MessageNumber);
+    }
+    
+    [Fact]
+    public void ValidateModelInYaml_MalformedAttributeValue_Fail()
+    {
+        var schemaValidator = new RtSchemaValidator();
+
+        string filePath = "sampleData/files/malformedAttributeValue.yaml";
+        var stream = File.OpenRead(filePath);
+        var operationResult = new OperationResult();
+        var isValid = schemaValidator.ValidateModelInYaml(stream, filePath, operationResult);
+        Assert.False(isValid);
+        Assert.Single(operationResult.Messages);
+        Assert.True(operationResult.HasErrors);
+        Assert.False(operationResult.HasFatalErrors);
+        Assert.Equal(1, operationResult.Messages[0].MessageNumber);
+    }
+}

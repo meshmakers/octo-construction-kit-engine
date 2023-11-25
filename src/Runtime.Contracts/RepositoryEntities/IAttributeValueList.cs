c9521fa33e7f53d34b3b60fd315ce221eb6c1edb
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-11-25 12:50:19 +0100

    Added RemoveAll method

--- src/Runtime.Contracts/RepositoryEntities/IAttributeValueList.cs
+++ src/Runtime.Contracts/RepositoryEntities/IAttributeValueList.cs
@@ -6,4 +6,12 @@ namespace Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
 /// <typeparam name="TValue"></typeparam>
 public interface IAttributeValueList<TValue> : IList<TValue>
 {
+    
+    /// <summary>
+    /// This method removes all items which matches the predicate.
+    /// The complexity is O(n).
+    /// </summary>
+    /// <param name="match">The match delegate function</param>
+    /// <returns>Amount of matches</returns>
+    int RemoveAll(Predicate<TValue> match);
 }

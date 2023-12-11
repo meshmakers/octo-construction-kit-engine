4695e69092450aa735feed617676da0c3f107f1d
Gerald Lochner <gerald.lochner@meshmakers.io>
2023-12-11 08:40:55 +0100

    Added statistics functions from main.

---
+++ src/Runtime.Engine/Repositories/Query/RtStatisticFunctions.cs
@@ -0,0 +1,162 @@
+using Meshmakers.Octo.ConstructionKit.Contracts.DependencyGraph;
+using Meshmakers.Octo.Runtime.Contracts;
+using Meshmakers.Octo.Runtime.Contracts.Repositories;
+using Meshmakers.Octo.Runtime.Contracts.Repositories.Query;
+using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
+
+namespace Meshmakers.Octo.Runtime.Engine.Repositories.Query;
+
+/// <summary>
+/// Implements the statistic functions for a result set.
+/// </summary>
+/// <typeparam name="TEntity"></typeparam>
+public class RtStatisticFunctions<TEntity> where TEntity : RtEntity
+{
+    private readonly CkTypeGraph _ckTypeGraph;
+    private readonly FieldGroupBy _groupBy;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="ckTypeGraph">The type graph</param>
+    /// <param name="groupBy">The grouping information</param>
+    public RtStatisticFunctions(CkTypeGraph ckTypeGraph, FieldGroupBy groupBy)
+    {
+        _ckTypeGraph = ckTypeGraph;
+        _groupBy = groupBy;
+    }
+
+    /// <summary>
+    /// Calculates the grouping result
+    /// </summary>
+    /// <param name="resultList">Result list the grouping needs to be executed.</param>
+    /// <returns></returns>
+    public IEnumerable<GroupingResult> Calculate(IEnumerable<TEntity> resultList)
+    {
+        var groupByPropertiesResult = resultList.GroupBy(g => new Key(_groupBy.GroupByAttributeNameList.Select(a => GetValue(a)(g))));
+
+        List<GroupingResult> calculateGrouping = new List<GroupingResult>();
+        foreach (IGrouping<Key, TEntity> entityGrouping in groupByPropertiesResult.OrderBy(x => x.Key))
+        {
+            var grouping = new GroupingResult(
+                _groupBy.GroupByAttributeNameList,
+                entityGrouping.Key.Keys,
+                entityGrouping.Count(),
+                RunStatistics(_groupBy.CountAttributeNameList,
+                    attributeName => entityGrouping.Count(x => GetValue(attributeName)(x) != null)),
+                RunStatistics(_groupBy.MinValueAttributeNameList,
+                    attributeName => entityGrouping.Min(x => GetValue(attributeName)(x))),
+                RunStatistics(_groupBy.MaxValueAttributeNameList,
+                    attributeName => entityGrouping.Max(x => GetValue(attributeName)(x))),
+                RunStatistics(_groupBy.AvgAttributeNameList,
+                    attributeName => entityGrouping.Average(x => (decimal?)GetValue(attributeName)(x)))
+            );
+            calculateGrouping.Add(grouping);
+        }
+
+        return calculateGrouping;
+    }
+
+    private static List<StatisticsResult> RunStatistics(IEnumerable<string>? attributeNames, Func<string, object?> calcFunction)
+    {
+        List<StatisticsResult> list = new List<StatisticsResult>();
+        if (attributeNames != null)
+        {
+            foreach (var attributeName in attributeNames)
+            {
+                list.Add(new StatisticsResult
+                {
+                    AttributeName = attributeName,
+                    Value = calcFunction(attributeName)
+                });
+            }
+        }
+
+        return list;
+    }
+
+    private Func<TEntity, object?> GetValue(string propertyName) =>
+        entity =>
+        {
+            if (_ckTypeGraph.AllAttributesByName.TryGetValue(propertyName, out var attributeCacheItem))
+            {
+                return AttributeValueConverter.ConvertAttributeValue(attributeCacheItem.ValueType, entity.Attributes[propertyName]);
+            }
+
+            throw RuntimeRepositoryException.AttributeWithNameDoesNotExist(_ckTypeGraph.CkTypeId, propertyName);
+        };
+
+    private class Key : IComparable
+    {
+        private readonly IEnumerable<object?> _keys;
+
+        public Key(IEnumerable<object?> keys)
+        {
+            _keys = keys;
+        }
+
+        public IEnumerable<object?> Keys => _keys;
+
+        public override bool Equals(object? obj)
+        {
+            var t = obj is Key other && _keys.SequenceEqual(other._keys);
+            return t;
+        }
+
+        public override int GetHashCode()
+        {
+            return _keys.Aggregate(0, (current, key) => current ^ key?.GetHashCode() ?? 0);
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj == null)
+            {
+                return 1;
+            }
+
+            if (!(obj is Key key1))
+            {
+                return 1;
+            }
+
+            var keys = _keys.ToArray();
+            var otherKeys = key1._keys.ToArray();
+            if (keys.Length < otherKeys.Length)
+            {
+                return -1;
+            }
+
+            if (keys.Length > otherKeys.Length)
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < keys.Length; i++)
+            {
+                var key = keys[i];
+                var otherKey = otherKeys[i];
+
+                if (key == null && otherKey != null)
+                {
+                    return -1;
+                }
+
+                if (key is IComparable comparable)
+                {
+                    var result = comparable.CompareTo(otherKey);
+                    if (result != 0)
+                    {
+                        return result;
+                    }
+                }
+                else
+                {
+                    throw RuntimeRepositoryException.NotComparable(key);
+                }
+            }
+
+            return 0;
+        }
+    }
+}

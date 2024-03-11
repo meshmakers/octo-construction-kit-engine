8f47dbe0608ffe9d88d5ff04fa539c0d0595f640
Philipp Schwaab <fhs48044@fh-salzburg.ac.at>
2024-03-11 15:35:06 +0100

    Call AddDescription in GenerateMermaidTextOutput

--- src/ConstructionKit.Compiler/Commands/Implementations/GenerateDocsCommand.cs
+++ src/ConstructionKit.Compiler/Commands/Implementations/GenerateDocsCommand.cs
@@ -531,10 +531,12 @@ public class GenerateDocsCommand : Command<OctoToolOptions>
 
         using StreamWriter outputFile = new(LinkHelpers.GetGeneratedFilePath(docPath, ckModelId, "index"));
 
-
+        //Create Page Heading (could be delegated to function)
         string[] split = ckModelId.ModelId.Split('.');
         await outputFile.WriteLineAsync($"# {split.Last()}");
         await outputFile.WriteLineAsync();
+
+        await AddDescription(outputFile, "DIAGRAM DESCRIPTION");
 
         await GenerateMermaidBoilerplate(ckModelId.SemanticVersionedFullName, outputFile);
 

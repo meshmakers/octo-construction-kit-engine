30dadaeaa828914ca401c6f3c64331f3587e0590
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-05-15 07:59:06 +0200

    #1079: New: Moved model state from Mongodb implementation to the basic implementation

---
+++ src/ConstructionKit.Contracts/DataTransferObjects/ModelState.cs
@@ -0,0 +1,17 @@
+﻿namespace Meshmakers.Octo.ConstructionKit.Contracts.DataTransferObjects;
+
+/// <summary>
+/// Defines the state of the construction kit model
+/// </summary>
+public enum ModelState
+{
+    /// <summary>
+    /// The model is importing
+    /// </summary>
+    Importing = 0,
+    
+    /// <summary>
+    /// The model is available
+    /// </summary>
+    Available = 1
+}

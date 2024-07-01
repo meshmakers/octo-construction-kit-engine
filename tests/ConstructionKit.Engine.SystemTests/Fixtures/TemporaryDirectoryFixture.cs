b9dff1c87ab022e7bdb10d3d01d72e63c0c9711f
Philipp Schwaab <fhs48044@fh-salzburg.ac.at>
2024-07-01 15:27:01 +0200

    AB#1317: Add System Testing for ContentGenerator.cs

---
+++ tests/ConstructionKit.Engine.SystemTests/Fixtures/TemporaryDirectoryFixture.cs
@@ -0,0 +1,29 @@
+﻿using ConstructionKit.Engine.SystemTests.Fixtures;
+
+namespace ConstructionKit.Engine.SystemTests.Documentation;
+
+public class TemporaryDirectoryFixture : ServiceCollectionFixture, IDisposable
+{
+    private readonly List<string> _tempDirectoryList;
+
+    public TemporaryDirectoryFixture()
+    {
+        _tempDirectoryList = new List<string>();
+    }
+
+    public void Dispose()
+    {
+        foreach (var tempDirectoryPath in _tempDirectoryList)
+        {
+            Directory.Delete(tempDirectoryPath, true);
+        }
+    }
+
+    public string CreateTempDirectory()
+    {
+        var tempDirectoryPath = Path.Combine(Path.GetTempPath(), "CkModelCompilerTests", Guid.NewGuid().ToString("N"));
+        Directory.CreateDirectory(tempDirectoryPath);
+        _tempDirectoryList.Add(tempDirectoryPath);
+        return tempDirectoryPath;
+    }
+}

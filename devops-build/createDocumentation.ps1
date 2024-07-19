6a50ac3f6f9e027eda41a39da976dfd7f03916f9
Philipp Schwaab <fhs48044@fh-salzburg.ac.at>
2024-07-19 07:08:40 +0200

    AB#1319: Testing Src TLD

--- devops-build/createDocumentation.ps1
+++ devops-build/createDocumentation.ps1
@@ -59,6 +59,6 @@ $version = "1.0"
 $linkPath = "/docs/technologyGuide/constructionKits/"
 callCompilerCommand -commandName $commandName -sourcePath $sourcePath -outputPath $outputPath -version $version -linkPath $linkPath
 
-Write-Host "Checking Available Directories in "$modulePath/../src""
-cd "$modulePath/../src"
+Write-Host "Checking Available Directories in "$modulePath/..""
+cd "$modulePath/.."
 ls

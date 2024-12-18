03d5620fbfe4b7dbedfc2c979f394472545bf8d3
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-12-18 10:19:38 +0100

    AB#1641: New: RtRecords are now correctly deserialized from RtEntities using newtonsoft json

--- src/Runtime.Contracts/EntityUpdateInfo.cs
+++ src/Runtime.Contracts/EntityUpdateInfo.cs
@@ -4,39 +4,6 @@ using Meshmakers.Octo.Runtime.Contracts.RepositoryEntities;
 namespace Meshmakers.Octo.Runtime.Contracts;
 
 /// <summary>
-///     Interface for an entity update info.
-/// </summary>
-/// <typeparam name="TEntity"></typeparam>
-public interface IEntityUpdateInfo<out TEntity> where TEntity : RtEntity
-{
-    /// <summary>
-    ///     Entity for modification.
-    /// </summary>
-    public TEntity? RtEntity { get; }
-
-    /// <summary>
-    ///     Runtime Identifier of an existing entity.
-    /// </summary>
-    public OctoObjectId? RtId { get; }
-    
-    /// <summary>
-    ///     Construction Kit Type Identifier of entity to be modified.
-    /// </summary>
-    public CkId<CkTypeId> CkTypeId { get; }
-
-    /// <summary>
-    ///     MOD option.
-    /// </summary>
-    public EntityModOptions ModOption { get; }
-
-    /// <summary>
-    /// Gets the runtime entity identifier.
-    /// </summary>
-    /// <returns></returns>
-    RtEntityId GetRtEntityId();
-}
-
-/// <summary>
 ///     Represents an entity update info.
 /// </summary>
 /// <typeparam name="TEntity"></typeparam>

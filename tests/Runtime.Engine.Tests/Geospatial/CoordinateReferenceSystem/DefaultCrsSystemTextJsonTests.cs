5334118a5df66f3d82241123a95a674d26e86e1b
Gerald Lochner <gerald.lochner@meshmakers.io>
2025-01-17 17:53:25 +0100

    Revert "AB#1405: Removed test - double has ranges"
    This reverts commit 5bcf0af1d217fbf6e78a115c9615f489f3f83978.

--- tests/Runtime.Engine.Tests/Geospatial/CoordinateReferenceSystem/DefaultCrsSystemTextJsonTests.cs
+++ tests/Runtime.Engine.Tests/Geospatial/CoordinateReferenceSystem/DefaultCrsSystemTextJsonTests.cs
@@ -41,4 +41,16 @@ public class DefaultCrsSystemTextJsonTests
         Assert.Equal(CRSType.Name, point.CRS.Type);
     }
 
+    [Fact]
+    public void Can_Serialize_CRS_issue_89()
+    {
+        var expected =
+            "{\"type\":\"Point\",\"coordinates\":[34.56,12.34],\"crs\":{\"properties\":{\"name\":\"TEST NAME\"},\"type\":\"name\"}}";
+        var point = new Point(new Position(12.34, 34.56)) { CRS = new NamedCRS("TEST NAME") };
+
+        var json = JsonSerializer.Serialize(point);
+
+        Assert.NotNull(json);
+        Assert.Equal(expected, json);
+    }
 }

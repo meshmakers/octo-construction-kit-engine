f68ef304ad25faaf19f242eda24c0c09deedfe37
reikla <reikla@users.noreply.github.com>
2025-09-22 12:13:06 +0200

    AB#2440 New: Add 2 new index types Unique and UniqueNotDeleted (#134)

--- src/ConstructionKit.Contracts/DataTransferObjects/IndexTypeDto.cs
+++ src/ConstructionKit.Contracts/DataTransferObjects/IndexTypeDto.cs
@@ -18,5 +18,15 @@ public enum IndexTypeDto
     /// <summary>
     ///     Full text search index.
     /// </summary>
-    Text = 2
+    Text = 2,
+
+    /// <summary>
+    ///     Unique index. Ensures the indexed attribute value is unique across all entities of this type.
+    /// </summary>
+    Unique = 3,
+
+    /// <summary>
+    ///     Unique index for non-deleted entities. Ensures uniqueness only when RtState is not set (default) or when RtState != 1 (not deleted).
+    /// </summary>
+    UniqueNotDeleted = 4
 }
